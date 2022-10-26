d01ac9ab7a6fef863301cfd08c29d5c3d085d1d4
hon <lee_ganghoon@naver.com>
2022-10-26 17:14:30 +0900

    동일. 0514

--- Step05_Constructor/src/test/main/MainClass01.java
+++ Step05_Constructor/src/test/main/MainClass01.java
@@ -8,8 +8,8 @@ public class MainClass01 {
 		//Student 클래스의 기본 생성자를 호출해서 객체를 생성하고
 		// 참조값을 s1이라는 이름의 지역변수에 담기
 		Student s1= new Student();
-	
 
+		
 	}
 
 }
--- Step05_Constructor/src/test/main/MainClass04.java
+++ Step05_Constructor/src/test/main/MainClass04.java
@@ -8,8 +8,17 @@ public class MainClass04 {
 		 *  hint ) Randdom 클래스를 import 해서 사용해 보세요.
 		 */
 
+		//Random 객체를 생성해서
 		Random m1 = new Random();
+		
+		//nextInt() 메소드를호출해서 정수 하나를 리턴 받아서 콘솔창에 출력해보기
 		System.out.println(m1.nextInt(10));
 		
+		//1~45 사이의 랜덤한 정수 하나를 얻어내서 ranNum2 라는 지역 변수에 담아 보세요.
+		
+		//true or false 중에 랜덤한 boolean 값을 얻어내서 isRun 이라는 지역 변수에 담아 보세요.
+		
+		int ranNum2 = m1.nextInt(1,46);
+		boolean inRun = m1.nextBoolean();
 	}
 }
